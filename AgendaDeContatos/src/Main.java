e477df4cdcbb0d6292d086f968b2d52b35b5361d
Ícaro Lima <icaro.lima@ccc.ufcg.edu.br>
2017-05-27 23:41:43 -0300

    Adotada formatação no estilo java.

--- AgendaDeContatos/src/Main.java
+++ AgendaDeContatos/src/Main.java
@@ -2,58 +2,59 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
 
-		Agenda agenda = new Agenda(100);
+	Agenda agenda = new Agenda(100);
 
-		System.out.print(Menu.getTextoMenuDeOpcoes());
+	System.out.print(Menu.getTextoMenuDeOpcoes());
 
-		String token;
-		while (!(token = sc.nextLine()).equals("S")) {
-			if (token.equals("C")) {
-				System.out.print(Utilidades.FIM_DE_LINHA + Menu.getTextoCadastrarContatoPosicao());
-				int posicao = Integer.parseInt(sc.nextLine());
+	String token;
+	while (!(token = sc.nextLine()).equals("S")) {
+	    if (token.equals("C")) {
+		System.out.print(Utilidades.FIM_DE_LINHA + Menu.getTextoCadastrarContatoPosicao());
+		int posicao = Integer.parseInt(sc.nextLine());
 
-				if (posicao > 0 && posicao <= agenda.getCapacidade()) {
-					System.out.print(Menu.getTextoCadastrarContatoNome());
-					String nome = sc.nextLine();
+		if (posicao > 0 && posicao <= agenda.getCapacidade()) {
+		    System.out.print(Menu.getTextoCadastrarContatoNome());
+		    String nome = sc.nextLine();
 
-					System.out.print(Menu.getTextoCadastrarContatoSobrenome());
-					String sobrenome = sc.nextLine();
+		    System.out.print(Menu.getTextoCadastrarContatoSobrenome());
+		    String sobrenome = sc.nextLine();
 
-					System.out.print(Menu.getTextoCadastrarContatoTelefone());
-					String telefone = sc.nextLine();
+		    System.out.print(Menu.getTextoCadastrarContatoTelefone());
+		    String telefone = sc.nextLine();
 
-					Contato contato = new Contato(nome, sobrenome, telefone);
+		    Contato contato = new Contato(nome, sobrenome, telefone);
 
-					agenda.cadastrarContato(contato, posicao);
+		    agenda.cadastrarContato(contato, posicao);
 
-					System.out.println(Menu.getTextoCadastrarContatoCadastroRealizado() + Utilidades.FIM_DE_LINHA);
-				} else {
-					System.out.println(Menu.getTextoPosicaoInvalida() + Utilidades.FIM_DE_LINHA);
-				}
-			} else if (token.equals("L")) {
-				System.out.println(Utilidades.FIM_DE_LINHA + agenda.toString());
-			} else if (token.equals("E")) {
-				System.out.print(Menu.getTextoExibirContato());
-				int posicao = Integer.parseInt(sc.nextLine());
-
-				if (agenda.validarPosicao(posicao)) {
-					Contato contato = agenda.obterContato(posicao);
-
-					System.out.println(Utilidades.FIM_DE_LINHA + contato.toString("%s %s - %s") + Utilidades.FIM_DE_LINHA);
-				} else {
-					System.out.println(Menu.getTextoPosicaoInvalida() + Utilidades.FIM_DE_LINHA);
-				}
-			} else {
-				System.out.println(Menu.getTextoOpcaoInvalida() + Utilidades.FIM_DE_LINHA);
-			}
-
-			System.out.print(Menu.getTextoMenuDeOpcoes());
+		    System.out.println(Menu.getTextoCadastrarContatoCadastroRealizado() + Utilidades.FIM_DE_LINHA);
+		} else {
+		    System.out.println(Menu.getTextoPosicaoInvalida() + Utilidades.FIM_DE_LINHA);
 		}
-		
-		sc.close();
+	    } else if (token.equals("L")) {
+		System.out.println(Utilidades.FIM_DE_LINHA + agenda.toString());
+	    } else if (token.equals("E")) {
+		System.out.print(Menu.getTextoExibirContato());
+		int posicao = Integer.parseInt(sc.nextLine());
+
+		if (agenda.validarPosicao(posicao)) {
+		    Contato contato = agenda.obterContato(posicao);
+
+		    System.out.println(
+			    Utilidades.FIM_DE_LINHA + contato.toString("%s %s - %s") + Utilidades.FIM_DE_LINHA);
+		} else {
+		    System.out.println(Menu.getTextoPosicaoInvalida() + Utilidades.FIM_DE_LINHA);
+		}
+	    } else {
+		System.out.println(Menu.getTextoOpcaoInvalida() + Utilidades.FIM_DE_LINHA);
+	    }
+
+	    System.out.print(Menu.getTextoMenuDeOpcoes());
 	}
+
+	sc.close();
+    }
 
 }
--- AgendaDeContatos/src/Menu.java
+++ AgendaDeContatos/src/Menu.java
@@ -1,40 +1,41 @@
 public class Menu {
 
-	public static String getTextoMenuDeOpcoes() {
-		return "(C)adastrar Contato" + Utilidades.FIM_DE_LINHA + "(L)istar Contatos" + Utilidades.FIM_DE_LINHA + "(E)xibir Contato"
-				+ Utilidades.FIM_DE_LINHA + "(S)air" + Utilidades.FIM_DE_LINHA + Utilidades.FIM_DE_LINHA + "Opção> ";
-	}
+    public static String getTextoMenuDeOpcoes() {
+	return "(C)adastrar Contato" + Utilidades.FIM_DE_LINHA + "(L)istar Contatos" + Utilidades.FIM_DE_LINHA
+		+ "(E)xibir Contato" + Utilidades.FIM_DE_LINHA + "(S)air" + Utilidades.FIM_DE_LINHA
+		+ Utilidades.FIM_DE_LINHA + "Opção> ";
+    }
 
-	public static String getTextoOpcaoInvalida() {
-		return "OPÇÃO INVÁLIDA!";
-	}
+    public static String getTextoOpcaoInvalida() {
+	return "OPÇÃO INVÁLIDA!";
+    }
 
-	public static String getTextoPosicaoInvalida() {
-		return "POSICAO INVÁLIDA!";
-	}
+    public static String getTextoPosicaoInvalida() {
+	return "POSICAO INVÁLIDA!";
+    }
 
-	public static String getTextoExibirContato() {
-		return "Contato> ";
-	}
+    public static String getTextoExibirContato() {
+	return "Contato> ";
+    }
 
-	public static String getTextoCadastrarContatoPosicao() {
-		return "Posição: ";
-	}
+    public static String getTextoCadastrarContatoPosicao() {
+	return "Posição: ";
+    }
 
-	public static String getTextoCadastrarContatoNome() {
-		return "Nome: ";
-	}
+    public static String getTextoCadastrarContatoNome() {
+	return "Nome: ";
+    }
 
-	public static String getTextoCadastrarContatoSobrenome() {
-		return "Sobrenome: ";
-	}
+    public static String getTextoCadastrarContatoSobrenome() {
+	return "Sobrenome: ";
+    }
 
-	public static String getTextoCadastrarContatoTelefone() {
-		return "Telefone: ";
-	}
+    public static String getTextoCadastrarContatoTelefone() {
+	return "Telefone: ";
+    }
 
-	public static String getTextoCadastrarContatoCadastroRealizado() {
-		return "CADASTRO REALIZADO!";
-	}
+    public static String getTextoCadastrarContatoCadastroRealizado() {
+	return "CADASTRO REALIZADO!";
+    }
 
 }
